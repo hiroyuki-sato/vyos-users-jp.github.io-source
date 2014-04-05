88e2a712e66c7f5e14059f556ce2dac598a634ce
higebu <yuya.kusakabe@gmail.com>
2014-04-06 04:50:24 +0900

    add fab deploy

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,15 @@
 from fabric.api import *
 import fabric.contrib.project as project
 import os
+import sys
+import SimpleHTTPServer
+import SocketServer
 
 # Local path configuration (can be absolute or relative to fabfile)
 env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
+env.publish_path = 'publish'
+PUBLISH_PATH = env.publish_path
 
 # Remote server configuration
 production = 'root@localhost:22'
@@ -20,6 +25,8 @@ def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
+    if os.path.isdir(PUBLISH_PATH):
+        local('rm -rf {publish_path}'.format(**env))
 
 def build():
     local('pelican -s pelicanconf.py')
@@ -32,7 +39,16 @@ def regenerate():
     local('pelican -r -s pelicanconf.py')
 
 def serve():
-    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+    os.chdir(env.deploy_path)
+
+    PORT = 8000
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
 
 def reserve():
     build()
@@ -58,3 +74,17 @@ def publish():
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
         delete=True
     )
+
+def deploy():
+    clean()
+    local('pelican -s publishconf.py')
+    local('git clone git@github.com:vyos-users-jp/vyos-users-jp.github.io.git publish')
+    local('cd {publish_path} && git pull'.format(**env))
+    for f in os.listdir(PUBLISH_PATH):
+        if os.path.isfile(f):
+            os.remove(f)
+    local('cp -pr {deploy_path}/. {publish_path}/'.format(**env))
+    local('cd {publish_path} && git add -A'.format(**env))
+    env.message = "Site updated"
+    local('cd {publish_path} && git commit -m \"{message}\"'.format(**env))
+    local('cd {publish_path} && git push origin master'.format(**env))
